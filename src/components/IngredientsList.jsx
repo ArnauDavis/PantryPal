552a7b16887f82de70b2774231b00a7f8d4ff3ca
Arnau Davis <arnau.davis@outlook.com>
2025-09-15 23:31:50 -0400

    Revert "added button to delete items on list"
    This reverts commit 6cf0c746d43bade3be42e75a0595a7bf0eab8a0f.

--- src/components/IngredientsList.jsx
+++ src/components/IngredientsList.jsx
@@ -2,23 +2,26 @@
 
     function IngredientsList(props){
       
-    const ingredientsListItems = props.ingredients.map(ingredient=> (
-        <li key={ingredient.key}><button type="button" className="removeButton" onClick={() => props.remove(ingredient.key)}>x</button>{ingredient.name}</li>
-    ))
+        const ingredientsListItems = props.ingredients.map(ingredient=> (
+            <li key={ingredient}>{ingredient}</li>
+        ))
 
-    return(    
-        <>
-        <section className="ingredientsContainer">
-            <h2>Ingredients on hand:</h2>
-            <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
-            {props.ingredients.length >3 ? <div className="get-recipe-container" ref={props.recipeSectionRef}>
-                <h3>Ready for a recipe?</h3>
-                <p>Generate a recipe from your list of ingredients.</p>
-                <button onClick={props.present}>Get a recipe</button>
-            </div> : null}
-        </section>
-        </>
-    )
-}
+        return(    
+            <>
+            <section className="ingredientsContainer">
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+                {props.ingredients.length >3 ? <div className="get-recipe-container">
+                    <div ref={props.ref}>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button onClick={props.present}>Get a recipe</button>
+                </div> : null}
+            </section>
+            </>
+                )
+
+    }
 
     export default IngredientsList
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,41 +1,37 @@
+
 import React from 'react'
 import IngredientsList from './IngredientsList'
 import Instructions from './Instructions'
 import ClaudeRecipe from './ClaudeRecipe'
 import { getRecipeFromMistral } from '../../ai' 
-import { v4 as uuidv4 } from 'uuid';
 
 
 function Main(){
+
+
     const [ingredients, setIngredients] = React.useState([])
     const [recipe, setRecipe] = React.useState('')
 
-    const recipeSectionRef = React.createRef()
+    const recipeSection = React.useRef(null)
 
     React.useEffect(()=>{
-        if(recipe !=="" && recipeSectionRef.current !== null){
-            recipeSectionRef.current.scrollIntoView({behavior: 'smooth'})
+        if(recipe !=="" && recipeSection.current !== null){
+            recipeSection.current.scrollIntoView({behavior: 'smooth'})
         }
     }, [recipe])
 
     function addIngredient(formData){
-        const newIngredient = {
-            key: uuidv4(),
-            name: formData.get('ingredient')
-        }
+        const newIngredient = formData.get('ingredient')
         setIngredients(prevIngredients => [...prevIngredients,newIngredient])
+        
     }
 
-    function removeItem(key){
-        const updatedList = ingredients.filter(ingredient => ingredient.key !== key)
-        setIngredients(updatedList)
-    }   
+     async function present(){
+           const generatedRecipe = await getRecipeFromMistral(ingredients)
+           setRecipe(generatedRecipe)
+        }
 
-    async function present(){
-        const generatedRecipe = await getRecipeFromMistral(ingredients.map(ingredient => ingredient.name))
-        setRecipe(generatedRecipe)
-    }
-
+   
     return(
         <main>
             <form action={addIngredient} className = 'add-ingredient-form'>
@@ -48,7 +44,7 @@ function Main(){
                 <button> Add ingredient</button>
             </form>
             {ingredients.length > 0 ?
-            <IngredientsList ingredients={ingredients} present={present} recipeSectionRef={recipeSectionRef} remove={removeItem}/> : null} 
+            <IngredientsList ingredients={ingredients} present={present} ref={recipeSection}/> : null} 
             {ingredients.length < 4 ? <Instructions/> : null}
             {recipe ? <ClaudeRecipe recipe={recipe}/> : null}
             
@@ -56,4 +52,5 @@ function Main(){
     )
 }
 
-export default Main 
+
+export default Main
